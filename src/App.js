9f9a00320a84a0bc4997586c0e29dd4499665387
Sudhir Vishwakarma <karmasudhir48@gmail.com>
2024-05-16 17:39:07 +0530

    changing text

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = ()=>{
     }
 
     let [state, setState] = useState({
-        text: "Hello My name is Sudhir and i Want to Join Google. Hello My name is Sudhia ndi Want toJoiGo ogle He llo My name is Sudhir and i Want to Join Google. ",
+        text: " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
         author: "Sudhir Vishwakarma",
     });
 
