b80bce523c2d585340da19a5a86de4b79f611f90
Sudhir Vishwakarma <karmasudhir48@gmail.com>
2024-05-16 13:16:03 +0530

    Initial Commit of Random User Generator React-JS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./app.css";
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ()=>{
+
+    let quotes = [];
+
+
+    async function loadQuotes(){
+        const quoote = await fetch('https://type.fit/api/quotes');
+        quotes = await quoote.json();
+        // console.log(quotes);
+    }
+
+    let [state, setState] = useState({
+        text: "Hello My name is Sudhir and i Want to Join Google. Hello My name is Sudhia ndi Want toJoiGo ogle He llo My name is Sudhir and i Want to Join Google. ",
+        author: "Sudhir Vishwakarma",
+    });
+
+    const newQuote = ()=>{
+        const random = quotes[Math.floor(Math.random()*quotes.length)];
+        setState(random);
+    }
+
+    loadQuotes();
+
+    return (
+        <>
+            <div className="main-container">
+                <div className="container">
+                    <h1>Quote Of The Day</h1>
+                    <div className="line"></div>
+                    <p className="quote">" {state.text} "</p>
+                    <p className="author">__ {state.author.split(',')[0]}</p>
+                    <button onClick={()=>{newQuote()}}>New Quote</button>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App;
